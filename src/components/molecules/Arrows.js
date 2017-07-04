97ec1bb108a2c0d68e29c9e1755baa400691ce9d
salsa <thibaud.salsa@example.com>
2017-07-04 17:13:11 +0200

    voila

--- src/components/molecules/Arrows.js
+++ src/components/molecules/Arrows.js
@@ -22,10 +22,10 @@ class Arrows extends Component {
     return (
 	    <div className="Arrows">
 	     <img src={space} alt="Lancer la lecture audio" />Lecture audio
-            <img src={up} alt="Aller au titre précédent" />Monter
-            <img src={down} alt="Aller au titre suivant" />Descendre
-            <img src={left} alt="Aller au titre suivant" />Titre précédent
-            <img src={right} alt="Aller sous-titre suivant" />Titre suivant
+            <img src={up} alt="Aller au titre précédent" />Titre suivant
+            <img src={down} alt="Aller au titre suivant" />Titre précédent
+            <img src={left} alt="Aller au titre suivant" />Monter dans le titre
+            <img src={right} alt="Aller sous-titre suivant" />Descendre dans le titre
       </div>
     );
   }
