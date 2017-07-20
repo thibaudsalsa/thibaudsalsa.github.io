be3bdc51f3c2ee68ed3865ee6f760318be5f8678
salsa <thibaud.salsa@example.com>
2017-07-20 13:18:32 +0200

    README

--- js_src/App.js
+++ js_src/App.js
@@ -1,2 +1,30 @@
-var body = document.body;
-consol.log(body);
+main();
+
+function MenuLeft()
+{
+    var MenuLeft = "<div id=MenuLeft>Test MenuLef</div>"
+    return (MenuLeft);
+}
+
+function MenuTop()
+{
+}
+
+function Document()
+{
+}
+
+function Parametre()
+{
+}
+
+function main()
+{
+    return
+    (
+	MenuLeft(),
+	MenuTop(),
+	Document(),
+	Parametre()
+    );
+}
