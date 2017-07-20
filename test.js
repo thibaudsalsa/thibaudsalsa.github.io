cc4849443ebcba95293755720dd1b7f879d5a766
Dondeo <robin.chatain@epitech.eu>
2017-07-20 15:42:43 +0200

    HTML UpBrowser Parser v1

--- test.js
+++ test.js
@@ -2,42 +2,46 @@
  * Created by Dondeo on 7/13/17.
  */
 
+var checkElem = ['P', 'H1', 'H2', 'H3', 'H4', 'H5', 'OL', 'LI'];
+
 function main() {
-    var childNode = document.body.childNodes;                       //listes des balises de premier rang
-    console.log(childNode);
-    var checkElem = ['P', 'H1', 'H2', 'H3', 'H4', 'H5', 'OL', 'LI'];//tableau des éléments à garder
-    var posCheckElem = -1;                                          //Valeur check d'éléments dans un élément
-    var upBrowserHTML = document.createElement("DIV");              //Où est stocké la nouvelle page
-    var bodyPart;                                                   //Résultat du parsing (1er rang)
-    var childPart;                                                  //Résultat du parsing (recursive)
+    var childNode = document.body.childNodes;
+    var bodyPart = document.createElement("BODY");
+    var childPart;
+
+    for(var i = 2; i < childNode.length; i++) {
+        if(childNode[i].nodeType === 1) {
+            if(childNode[i].hasChildNodes() === true) {
+                childPart = exploreNode(childNode[i]);
+                bodyPart.appendChild(childPart);
+            }
+        }
+    }
+    document.documentElement.removeChild(document.body);
+    document.documentElement.appendChild(bodyPart);
+}
+
+
+function exploreNode(myNode) {
+    var childNode = myNode.childNodes;
+    var posCheckElem = -1;
+    var bodyPart = document.createElement(myNode.nodeName);
+    var childPart;
 
     for(var i = 0; i < childNode.length; i++) {
         if(childNode[i].nodeType === 1) {
-            if(childNode[i].hasChildNodes() === true) {
-                childPart = exploreNodeTest(childNode[i]);
+            if (childNode[i].hasChildNodes() === true) {
+                childPart = exploreNode(childNode[i]);
             }
             posCheckElem = checkElem.indexOf(childNode[i].nodeName);
             if(posCheckElem !== -1) {
-                bodyPart = document.createElement(checkElem[posCheckElem]);
                 bodyPart.appendChild(childPart);
             }
-            else {
-                bodyPart = childPart;
-            }
-                upBrowserHTML.appendChild(bodyPart);
+        }
+        else if (childNode[i].nodeType === 3) {
+            childPart = document.createTextNode(childNode[i].nodeValue);
+            bodyPart.appendChild(childPart);
         }
     }
-    document.body.innerHTML = '';
-    document.body.appendChild(upBrowserHTML);
+    return (bodyPart);
 }
-
-function exploreNodeTest() {
-    var p = document.createElement("P");
-    var t = document.createTextNode("Je suis un test.");
-
-    p.appendChild(t);
-    return (p);
-}
-
-//function exploreNode(myNode) {
-//}
