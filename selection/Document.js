21dd2e809735df93613197c10488439988750927
salsa <thibaud.salsa@example.com>
2017-07-21 10:06:52 +0200

    voila

--- selection/Document.js
+++ selection/Document.js
@@ -1,39 +1,40 @@
 import Mousetrap from 'mousetrap'
 import keyboard  from './keyboard.js';
 
-this.typeSelected = "";
-this.initElements();
-this.voice = true;
-this.maxElements = 0;
-this.selectedText = "";
-this.synth = "";
-this.loading: true;
-this.selected: 0;
-this.selectAlled: 1;
-//this.typeSelected: "";
-this.htmlRender: "";
+
 
 
 function selection()
 {
+    this.typeSelected = "";
+    this.initElements();
+    this.voice = true;
+    this.maxElements = 0;
+    this.selectedText = "";
+    this.synth = "";
+    this.loading = true;
+    this.selected = 0;
+    this.selectAlled = 1;
+    this.htmlRender = "";
+
     context = this;
 
 
     // RIGHT KEY
     Mousetrap.bind(keyboard[4], () => {
-        let priorityOrder = ["h1", "h2", "h3", "h4", "h5", "p", "li"];
+        let priorityOrder = ["h1", "h2", "h3", "h4", "h5"];
         let found = false;
-        for(let j=0;(j<5)&&(found===false);j++)
+        for (let j=0;(j<5)&&(found===false);j++)
         {
-            for(let k=0;(k<context.elements[priorityOrder[j]].length)&&(found===false);k++)
+            for (let k=0;(k<context.elements[priorityOrder[j]].length)&&(found===false);k++)
             {
-                if(context.elements[priorityOrder[j]][k] === context.selected + 1 || context.selected >= context.maxElements - 1)
+                if (context.elements[priorityOrder[j]][k] === context.selected + 1 || context.selected >= context.maxElements - 1)
                 {
                     found = true
                 }
             }
         }
-    if(found===false)
+    if (found===false)
     {
         this.selected = context.selected + 1;
         this.selectAlled = context.selected + 1;
@@ -45,19 +46,19 @@ function selection()
 
     //LEFT KEY
     Mousetrap.bind(keyboard[3], () => {
-        let priorityOrder = ["h1", "h2", "h3", "h4", "h5", "p", "li"];
-        let found = false;
-        for(let j=0;(j<5)&&(found===false);j++)
+        let priorityOrder = ["h1", "h2", "h3", "h4", "h5"];
+        let found = false
+        for (let j=0;(j<5)&&(found===false);j++)
         {
-            for(let k=0;(k<context.elements[priorityOrder[j]].length)&&(found===false);k++)
+            for (let k=0;(k<context.elements[priorityOrder[j]].length)&&(found===false);k++)
             {
-                if(context.elements[priorityOrder[j]][k] === context.selected)
+                if (context.elements[priorityOrder[j]][k] === context.selected)
                 {
                     found = true
                 }
             }
         }
-    if(found===false)
+    if (found===false)
     {
         this.selected = context.selected - 1;
         this.selectAlled = context.selected + 1;
@@ -72,13 +73,13 @@ function selection()
         let priorityOrder = ["h1", "h2", "h3", "h4", "h5", "p", "li"];
         let currentElement = -1;
         let found = false;
-        for(let i=1; (context.selected + i < context.maxElements-1)&&(found===false);i++)
+        for (let i=1; (context.selected + i < context.maxElements-1)&&(found===false);i++)
         {
-            for(let j=0;(j<5)&&(found===false);j++)
+            for (let j=0;(j<5)&&(found===false);j++)
             {
-                for(let k=0;(k<context.elements[priorityOrder[j]].length)&&(found===false);k++)
+                for (let k=0;(k<context.elements[priorityOrder[j]].length)&&(found===false);k++)
                 {
-                    if(context.elements[priorityOrder[j]][k] === context.selected + i)
+                    if (context.elements[priorityOrder[j]][k] === context.selected + i)
                     {
                         currentElement = context.selected + i;
                         found = true
@@ -86,7 +87,7 @@ function selection()
                 }
             }
         }
-    if(found===true)
+    if (found===true)
     {
         this.selected = currentElement;
         context.selectAll();
@@ -101,13 +102,13 @@ function selection()
         let priorityOrder = ["h1", "h2", "h3", "h4", "h5", "p", "li"];
         let currentElement = -1;
         let found = false;
-        for(let i=1; (context.selected - i >= 0)&&(found===false);i++)
+        for (let i=1; (context.selected - i >= 0)&&(found===false);i++)
         {
             for(let j=0;(j<5)&&(found===false);j++)
             {
-                for(let k=0;(k<context.elements[priorityOrder[j]].length)&&(found===false);k++)
+                for (let k=0;(k<context.elements[priorityOrder[j]].length)&&(found===false);k++)
                 {
-                    if(context.elements[priorityOrder[j]][k] === context.selected - i)
+                    if (context.elements[priorityOrder[j]][k] === context.selected - i)
                     {
                         currentElement = context.selected - i;
                         found = true
@@ -141,10 +142,4 @@ function selection()
     }
 }
 
-/*
-function new_selected_elem()
-{
-    document.getElementsByClassName("selected").className = ""; //l'élément selectionée devien une classe nul
-    context.state.selected.getElementsByTagName("poulet").className = "selected"; //changement de classe
-}
-*/
+//ne pas recreer la page, donc modifier la selection dans la fonction de detection des touches
