cbeca6a8c5bb88658807965836a6ff980c87c234
Dondeo <robin.chatain@epitech.eu>
2017-07-26 13:34:59 +0200

    pwet

--- test.js
+++ test.js
@@ -2,44 +2,52 @@
  * Created by Dondeo on 7/13/17.
  */
 
-
+//Balises gérés par le script
 var checkElem = ['BODY', 'P', 'H1', 'H2', 'H3', 'H4', 'H5', 'OL', 'LI', 'A', 'IMG'];
 
+// Parsing général du fichier HTML
 function f1() {
     var childNode = document.body.childNodes;
     var newBody = document.createElement("BODY");
     var childLength = childNode.length;
     var childPart;
-//    var newHead = document.createElement("HEAD");
+//    var newHead = document.createElement("HEAD"); /* soon */
 
-
-    for(var i = 2; i < childLength; i++) {
+//Parcours du body actuel
+    for(var i = 2; i < childLength; i++) { //i a modifier pour version finale
         if(childNode[i].nodeType === 1) {
+            //parsing entier d'une balise du body
             childPart = f2(childNode[i], newBody, newBody);
+            //Ajoute la balise si ce n'est pas déjà le cas
             if (newBody !== childPart) {
                 newBody.appendChild(childPart);
             }
         }
     }
-    newBody.firstElementChild.setAttribute("id", "selected");
+    //remplace le body atuel par le nouveau body
+    newBody.firstElementChild.setAttribute("class", "selected");
     document.documentElement.removeChild(document.body);
     document.documentElement.appendChild(newBody);
     f5();
 }
 
+//Parsing de la balise myNode
 function f2(myNode, newBody, parentBody) {
     var bodyPart = f3(myNode, newBody, parentBody);
 
     return (f4(bodyPart, myNode, newBody));
 }
 
+//Création de la balise
 function f3(myNode, newBody, parentBody) {
     var tempNode = myNode;
     var bodyPart;
 
+    //On remonte au parent tant que la balise visualisé n'est pas une balise valide
     while (checkElem.indexOf(tempNode.nodeName) === -1) {
         tempNode = tempNode.parentNode;
     }
+    //La balise envoyé en paramètre est valide : Création d'une nouvelle balise
     if (tempNode.nodeName.localeCompare(myNode.nodeName) === 0) {
         bodyPart = document.createElement(myNode.nodeName);
         if (tempNode.nodeName.localeCompare("A") === 0 && tempNode.hasAttribute("href")){
@@ -90,9 +98,6 @@ function f5() {
     my_css.setAttribute("href", "selection/index.css");
     document.head.appendChild(my_css);
 
-//    var elmt = document.getElementById("selected");
-//    elmt.style.backgroundColor = "#c0c0c0";
-//    elmt.style.color = "#ff0000";
 
     var my_script = document.createElement("SCRIPT");
     my_script.setAttribute("src", "./key.js");
