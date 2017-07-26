04bcfb53348bbe0fec582b732ca1226107876701
salsa <thibaud.salsa@example.com>
2017-07-26 14:07:27 +0200

    voila

--- key.js
+++ key.js
@@ -74,13 +74,13 @@ function applyKey (_event_){
     if ( intKeyCode == KEY_RIGHT)
     {
 	var childNode = document.body.childNodes;
-        for (var i = 0; 'selected' != childNode[i].id; i++)
+        for (var i = 0; 'selected' != childNode[i].className; i++)
         {
         }
         if (i+1 < childNode.length-1 && childNode[i + 1].nodeName[0] !== 'H')
         {
-	    childNode[i].id = '';
-            childNode[i + 1].id = 'selected';
+	    childNode[i].className = '';
+            childNode[i + 1].className = 'selected';
         }
 	winObj.keyCode = intKeyCode = REMAP_KEY_T;
 	winObj.returnValue = false;
@@ -90,13 +90,13 @@ function applyKey (_event_){
     if (intKeyCode == KEY_LEFT )
     {
 	var childNode = document.body.childNodes;
-        for (var i = 0; 'selected' != childNode[i].id; i++)
+        for (var i = 0; 'selected' != childNode[i].className; i++)
         {
         }
         if (i-1 >= 0 && childNode[i-1].nodeName[0] !== 'H' && childNode[i].nodeName[0] !== 'H')
         {
-	    childNode[i].id = '';
-            childNode[i - 1].id = 'selected';
+	    childNode[i].className = '';
+            childNode[i - 1].className = 'selected';
         }
 	winObj.keyCode = intKeyCode = REMAP_KEY_T;
 	winObj.returnValue = false;
@@ -107,7 +107,7 @@ function applyKey (_event_){
     {
 	var temp = 0;
 	var childNode = document.body.childNodes;
-	for (var i = 0; 'selected' != childNode[i].id; i++)
+	for (var i = 0; 'selected' != childNode[i].className; i++)
 	{
         }
         temp = i;
@@ -115,12 +115,23 @@ function applyKey (_event_){
 	{
 	    i--;
 	}
+	for(let tmp2=0; tmp2<childNode.length; tmp2++)
+	{
+	    if (childNode[i].className === 'selected')
+		childNode[i].className = 'nop';
+	}
         for (;i < childNode.length; i--)
         {
             if (childNode[i].nodeName[0] === 'H')
             {
-		childNode[temp].id = '';
-                childNode[i].id = 'selected';
+		childNode[temp].className = 'nop';
+		temp = temp + 1;
+		for (;childNode[temp].nodeName[0] !== 'H'; temp++)
+		    childNode[temp].className = 'nop';
+                childNode[i].className = 'selected';
+		i = i + 1;
+		for (;childNode[i].nodeName[0] !== 'H'; i++)
+		    childNode[i].className = 'selected';
                 break;
             }
         }
@@ -134,17 +145,23 @@ function applyKey (_event_){
 	var found = false
         var temp = 0;
         var childNode = document.body.childNodes;
-        for (var i = 0; 'selected' != childNode[i].id; i++)
+        for (var i = 0; 'selected' != childNode[i].className; i++)
         {
         }
-        temp = i;
+	temp = i;
         i++;
         for (;i < childNode.length; i++)
         {
             if (childNode[i].nodeName[0] === 'H')
             {
-		childNode[temp].id = '';
-                childNode[i].id = 'selected';
+		childNode[temp].className = 'nop';
+		temp = temp + 1;
+		for (;childNode[temp].nodeName[0] !== 'H'; temp++)
+		    childNode[temp].className = 'nop';
+                childNode[i].className = 'selected';
+		i = i + 1;
+		for (;childNode[i].nodeName[0] !== 'H'; i++)
+		    childNode[i].className = 'selected';
                 break;
             }
         }
