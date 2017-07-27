aabad1603d50eca3d905c64783a29c332681156a
salsa <thibaud.salsa@example.com>
2017-07-27 13:45:14 +0200

    voila

--- key.js
+++ key.js
@@ -196,4 +196,92 @@ function applyKey (_event_){
 	winObj.returnValue = false;
 	return false;
     }
+
+    if ( intKeyCode == KEY_L)
+    {
+	var temp = 1;
+	var childNode = document.body.childNodes;
+	for (var i = 1; 'selected' != childNode[i].className; i++)
+	{
+        }
+	if (i != 0 && i != 1)
+	{
+        temp = i;
+	var current_title = childNode[i].nodeName[1];
+	if (i != 1)
+	{
+	    i--;
+	}
+	for(let tmp2=1; tmp2<childNode.length; tmp2++)
+	{
+	    if (childNode[i].className === 'selected')
+		childNode[i].className = 'nop';
+	}
+        for (;i < childNode.length; i--)
+        {
+            if (childNode[i].nodeName[0] === 'H' && childNode[i].nodeName[1] < current_title)
+            {
+		childNode[temp].className = 'nop';
+		temp = temp + 1;
+		while (temp < childNode.length && childNode[temp].nodeName[0] !== 'H')
+		{
+		//for (;childNode[temp].nodeName[0] !== 'H'; temp++)
+		    childNode[temp].className = 'nop';
+		    temp = temp + 1;
+		}
+                childNode[i].className = 'selected';
+		i = i + 1;
+		while (i < childNode.length && childNode[i].nodeName[0] !== 'H')
+		{
+		    //for (;childNode[i].nodeName[0] !== 'H'; i++)
+		    childNode[i].className = 'selected';
+		    i = i + 1;
+		}
+                break;
+            }
+        }
+	}
+	winObj.keyCode = intKeyCode = REMAP_KEY_T;
+	winObj.returnValue = false;
+	return false;
+    }
+
+    if (intKeyCode == KEY_M)
+    {
+	var found = false
+        var temp = 0;
+        var childNode = document.body.childNodes;
+        for (var i = 0; 'selected' != childNode[i].className; i++)
+        {
+        }
+	temp = i;
+	var current_title = childNode[i].nodeName[1];
+        i++;
+        for (;i < childNode.length; i++)
+        {
+            if (childNode[i].nodeName[0] === 'H' && childNode[i].nodeName[1] > current_title)
+            {
+		childNode[temp].className = 'nop';
+		temp = temp + 1;
+		while (temp < childNode.length && childNode[temp].nodeName[0] !== 'H')
+		{
+		//for (;childNode[temp].nodeName[0] !== 'H'; temp++)
+		    childNode[temp].className = 'nop';
+		    temp = temp + 1;
+		}
+                childNode[i].className = 'selected';
+		i = i + 1;
+		while (i < childNode.length && childNode[i].nodeName[0] !== 'H')
+		{
+		    //for (;childNode[i].nodeName[0] !== 'H'; i++)
+		    childNode[i].className = 'selected';
+		    i = i + 1;
+		}
+                break;
+            }
+        }
+	winObj.keyCode = intKeyCode = REMAP_KEY_T;
+	winObj.returnValue = false;
+	return false;
+    }
 }
